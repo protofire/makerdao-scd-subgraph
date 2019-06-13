9f6e0ec0ffce81c371f91df8b06bccd8c0f5571c
Sebastián Galiano <sgaliano@gmail.com>
2019-06-13 17:15:27 -0300

    Fix addresses decoding

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,10 +1,10 @@
-import { Address, BigDecimal, BigInt, Bytes } from '@graphprotocol/graph-ts'
+import { Address, BigDecimal, BigInt, Bytes, log } from '@graphprotocol/graph-ts'
 
 let PRECISION = BigDecimal.fromString('1000000000000000000') // 10^18
 export let ZERO = BigDecimal.fromString('0')
 
-export function toAddress(value: Bytes): Bytes {
-  return Address.fromHexString(value.toHex())
+export function toAddress(value: Bytes): Address {
+  return Address.fromHexString(value.toHex()).subarray(-20) as Address
 }
 
 export function toBigInt(value: Bytes): BigInt {
